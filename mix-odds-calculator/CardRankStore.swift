b5e8a31eb10e71b6ca934b26abc991012bfb0018
Masashi Tsubosaka <m.tsubosaka@gmail.com>
2022-03-30 23:35:00 +0900

    ポーカーの役の組み合わせをあらかじめ計算するクラスの追加

---
+++ mix-odds-calculator/CardRankStore.swift
@@ -0,0 +1,154 @@
+//
+//  CardRankStore.swift
+//  あらかじめあり得る種類のカードの5枚の組み合わせに対する役判定を前計算しておき、
+//  5枚のカードの組み合わせが与えられた時にO(1)で返すためのクラス
+//
+//  Created by 坪坂正志 on 2022/03/14.
+//
+
+import Foundation
+import Algorithms
+
+
+final public class CardRankStore{
+    private var cardPrimes = [0, 0, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+    private var pokerHandDictNoFlash : Dictionary<Int, Hand>
+    private var pokerHandDictFlash : Dictionary<Int, Hand>
+    public static let shared = CardRankStore()
+
+    private init(){
+        pokerHandDictNoFlash = [:]
+        pokerHandDictFlash = [:]
+        makeHandRankDict()
+    }
+    private func makeHandRankDict(){
+        // quads
+        for i in 2...14{
+            var quadVal = cardPrimes[i]
+            quadVal = quadVal * quadVal * quadVal * quadVal
+            for kicker in 2...14{
+                if(i == kicker){
+                    continue
+                }
+                let hv = i * 100 + kicker
+                pokerHandDictNoFlash[quadVal * cardPrimes[kicker]] = Hand(handRank: HandRank.quads.rawValue, handValue: hv)
+            }
+        }
+        // trips
+        for trips_v in 2...14{
+            var tripVal = cardPrimes[trips_v]
+            tripVal = tripVal * tripVal * tripVal
+            // full house
+            for pair_v in 2...14{
+                if(trips_v == pair_v){
+                    continue
+                }
+                let pairVal = cardPrimes[pair_v] * cardPrimes[pair_v]
+                let handVal = tripVal * pairVal
+                pokerHandDictNoFlash[handVal] = Hand(handRank: HandRank.fullHouse.rawValue, handValue: trips_v * 100 + pair_v)
+            }
+            for kicker1 in 2...13{
+                if(trips_v == kicker1){
+                    continue
+                }
+                for kicker2 in kicker1 + 1...14{
+                    if(trips_v == kicker2){
+                        continue
+                    }
+                    // kicker1 < kicker2
+                    let handVal = tripVal * cardPrimes[kicker1] * cardPrimes[kicker2]
+                    pokerHandDictNoFlash[handVal] = Hand(handRank: HandRank.trips.rawValue, handValue: trips_v * 10000 + kicker2 * 100 + kicker1)
+                }
+            }
+        }
+        // two pair
+        for pair1 in 2...14{
+            for pair2 in 2...14{
+                if(pair1 >= pair2){
+                    continue
+                }
+                var pairVal = cardPrimes[pair1] * cardPrimes[pair2]
+                pairVal = pairVal * pairVal
+                for kicker in 2...14{
+                    if(pair1 == kicker || pair2 == kicker){
+                        continue
+                    }
+                    let handVal = pairVal * cardPrimes[kicker]
+                    pokerHandDictNoFlash[handVal] = Hand(handRank: HandRank.twoPair.rawValue, handValue: pair2 * 10000 + pair1 * 100 + kicker)
+                }
+            }
+        }
+        // one pair
+        for pair in 2...14{
+            let pairVal = cardPrimes[pair] * cardPrimes[pair]
+            for kicker1 in 2...12{
+                if(pair == kicker1){
+                    continue
+                }
+                let pairVal2 = pairVal * cardPrimes[kicker1]
+                for kicker2 in kicker1 + 1...13{
+                    if(pair == kicker2){
+                        continue
+                    }
+                    let pairVal3 = pairVal2 * cardPrimes[kicker2]
+
+                    for kicker3 in kicker2 + 1...14{
+                        if(pair == kicker3){
+                            continue
+                        }
+                        let handVal = pairVal3 * cardPrimes[kicker3]
+                        let hv = pair * 1000000 + kicker1 * 10000 + kicker2 * 100 + kicker3
+                        pokerHandDictNoFlash[handVal] = Hand(handRank: HandRank.onePair.rawValue, handValue: hv)
+                    }
+                }
+            }
+        }
+        // straight
+        for high in 5...14{
+            var hv = 1
+            for i in high - 4 ... high{
+                if( i == 1){
+                    hv *= cardPrimes[14]
+                }else{
+                    hv *= cardPrimes[i]
+                }
+            }
+            pokerHandDictNoFlash[hv] = Hand(handRank: HandRank.straight.rawValue, handValue: high)
+            pokerHandDictFlash[hv] = Hand(handRank: HandRank.straightFlush.rawValue, handValue: high)
+        }
+        let cards = [2,3,4,5,6,7,8,9,10,11,12,13,14]
+        for card in cards.combinations(ofCount: 5){
+            var hv = 1
+            var hvalue = 0
+            for i in 0...4{
+                let ci = card[i]
+                hv *= cardPrimes[ci]
+                hvalue = card[4 - i] + hvalue * 20
+            }
+            if(pokerHandDictNoFlash[hv] != nil){
+                continue
+            }
+            pokerHandDictNoFlash[hv] = Hand(handRank: HandRank.highCard.rawValue, handValue: hvalue)
+            pokerHandDictFlash[hv] = Hand(handRank: HandRank.flush.rawValue, handValue: hvalue)
+        }
+    }
+    public func judgePokerHand(cards: [PlayingCard]) -> Hand{
+        let c0 = cards[0]
+        var isFlash = true
+        var hv = cardPrimes[c0.number]
+        for i in 1...4{
+            if(cards[i].suit.rawValue != c0.suit.rawValue){
+                isFlash = false
+            }
+            hv *= cardPrimes[cards[i].number]
+        }
+        if(pokerHandDictNoFlash[hv] == nil){
+            return Hand(handRank: 0, handValue: 0)
+        }
+        if(isFlash){
+            return pokerHandDictFlash[hv]!
+        }else{
+            return pokerHandDictNoFlash[hv]!
+        }
+    }
+}
---
+++ mix-odds-calculator/TestRankStoreView.swift
@@ -0,0 +1,80 @@
+//
+//  TestRankStoreView.swift
+//  mix-odds-calculator
+//
+//  Created by 坪坂正志 on 2022/03/30.
+//
+
+import SwiftUI
+
+public class TestRankObject : ObservableObject{
+    @Published var isError = false
+    @Published var errorMessage = ""
+    @Published var isCalculated = false
+    @Published var handValue = 0
+    @Published var handRank = 0
+    private func parsePlayingCard(playingCard: String) -> [PlayingCard]?{
+        let hand_arr: [String] = playingCard.components(separatedBy: " ")
+        var result: [PlayingCard] = []
+        if playingCard.isEmpty{
+            return result
+        }
+        for hstr in hand_arr {
+            let c = parseCard(card: hstr)
+            if(c == nil){
+                isError = true
+                errorMessage = hstr + "は不正な形式です"
+                return nil
+            }
+            result.append(c!)
+        }
+        return result
+    }
+    public func calculate(hand1: String){
+        isError = false
+        if(hand1.isEmpty){
+            isError = true
+            errorMessage = "Player1もしくはPlayer2のハンドが空です"
+            return
+        }
+        let hand1_arr = parsePlayingCard(playingCard: hand1)
+        let rankStore = CardRankStore.shared
+        let hand = rankStore.judgePokerHand(cards: hand1_arr!)
+        handValue = hand.handValue
+        handRank = hand.handRank
+        isCalculated = true
+    }
+    
+}
+struct TestRankStoreView: View {
+    @State private var hand1 = ""
+    @ObservedObject var model  = TestRankObject()
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Player1 Hand")
+                Spacer()
+            }
+            TextField("Ad 2h 3h 4c 5s", text: $hand1)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button(action: {
+                model.calculate(hand1: hand1)
+            }){
+                Text("計算")
+            }.alert(isPresented: $model.isError){
+                Alert(title: Text("エラー"), message: Text(model.errorMessage),
+                      dismissButton: .default(Text("OK")))
+            }
+            if(model.isCalculated){
+                Text("rank: " + String(model.handRank))
+                Text("value: " + String(model.handValue))
+            }
+        }
+    }
+}
+
+struct TestRankStoreView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestRankStoreView()
+    }
+}
